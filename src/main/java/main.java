b84f124a481fe69647e02fc295abfe140733e222
elarreglador <elarreglador@protonmail.com>
2024-10-25 17:44:18 +0200

    Agregada funcion containsTable que devuelve true/false si una tabla existe

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,38 +6,68 @@ import java.sql.SQLException;
 
 public class main {
 
-	public static void main(String[] args) {
-		String urlJDBC = "jdbc:sqlite:sample.db"; // Ruta de la base de datos
+	public static void main(String[] args) throws SQLException {
+		
+		//CONECTAMOS A LA BD Y CONSULTAMOS LA PERSONA DE UN CODIGO POSTAL
+		System.out.println("CONECTAMOS A LA BD Y CONSULTAMOS LA PERSONA DE UN CODIGO POSTAL");
+		String urlJDBC = "jdbc:sqlite:sample.db"; 
+		Connection con = DriverManager.getConnection(urlJDBC);
 
-		// Usamos try-with-resources para manejar conexiones y declaraciones
-		// automáticamente
-		try (	Connection con = DriverManager.getConnection(urlJDBC);
-				PreparedStatement pstmt = con.prepareStatement("SELECT nombre, apellido FROM personas WHERE cp = ?;")) {
-			
-			// Código postal a buscar
-            String codigoPostal = "28005";
-            pstmt.setString(1, codigoPostal);
+		// preparamos la consulta
+		PreparedStatement pstmt = con.prepareStatement("SELECT nombre, apellido FROM personas WHERE cp = ?;");
+        String codigoPostal = "28005";
+        pstmt.setString(1, codigoPostal);
 
-			// Ejecutar la consulta
-			try (ResultSet rs = pstmt.executeQuery()) {
-				// Procesar los resultados
-				while (rs.next()) {
-					String nombre = rs.getString("nombre");
-					String apellido = rs.getString("apellido");
-					System.out.println(nombre + " | " + apellido);
-				}
-			}
-		} catch (SQLException e) {
-			System.err.println("Error al conectar a la base de datos o ejecutar la consulta: " + e.getMessage());
+		// Ejecutar la consulta
+		ResultSet rs = pstmt.executeQuery();
+		// Listar el resultado
+		while (rs.next()) {
+			String nombre = rs.getString("nombre");
+			String apellido = rs.getString("apellido");
+			System.out.println(codigoPostal + " : " + nombre + " " + apellido);
 		}
+		System.out.println();
+		
+		
+		
+		
+		// CONSULTAMOS SI LA TABLA personas EXISTE EN NUESTRA BD
+		System.out.println("CONSULTAMOS SI LA TABLA personas EXISTE");
+		System.out.println(containsTable(con, "personas"));
+		
+		
 	}
 
 	
-	
-	
-	// Método para conectar (actualmente no implementado)
-	public static void containsTable(Connection con, String tableName) {
-		
+	// INDICA SI LA TABLA EXISTE EN NUESTRA BD
+	public static boolean containsTable(Connection con, String tableName) {
+		// Connection con = DriverManager.getConnection("jdbc:sqlite:sample.db");
+		// String tableName = "personas";
+
+		boolean retorno = false;
+
+		try (Connection conexion = con;) {
+			ResultSet respuesta = con.getMetaData().getTables(null, null, tableName, null);
+
+			if (respuesta.next()) {
+				// La tabla "+ tableName +" existe en la base de datos y movemos el cursor a la
+				// primera fila del ResultSet
+				do {
+					retorno = true;
+					// System.out.println("Catalogo: " + respuesta.getString(1));
+					// System.out.println("esquema: " + respuesta.getString(2));
+					// System.out.println("tabla: " + respuesta.getString(3));
+					// System.out.println("tipo: " + respuesta.getString(4));
+				} while (respuesta.next());
+			} else {
+				// La tabla "+ tableName +" no existe en la base de datos
+			}
+
+		} catch (SQLException e) {
+			System.err.println("EXCEPCION: " + e.getMessage());
+		}
+
+		return retorno;
 	}
 
 }
