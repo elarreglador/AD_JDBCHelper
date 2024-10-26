3b9b833de6b07d1cc19305fc3be035b11e9fb7e5
elarreglador <elarreglador@protonmail.com>
2024-10-26 10:44:12 +0200

    Limpieza de codigo y ajustes leves

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -15,14 +15,16 @@ public class main {
 		String urlJDBC = "jdbc:sqlite:sample.db";
 		Connection con = DriverManager.getConnection(urlJDBC);
 
+		
 		// CONSULTAMOS LA PERSONA DE UN CODIGO POSTAL
 		// preparamos la consulta
 		PreparedStatement pstmt = con.prepareStatement("SELECT nombre, apellido FROM personas WHERE cp = ?;");
 		String codigoPostal = "28002";
 		pstmt.setString(1, codigoPostal);
 
-		// Ejecutar la consulta
+		// Ejecutar la consulta y cerrar consulta
 		ResultSet rs = pstmt.executeQuery();
+		pstmt.close();
 
 		// Listar el resultado
 		while (rs.next()) {
@@ -32,21 +34,26 @@ public class main {
 		}
 		System.out.println();
 
+		
 		// CONSULTAMOS SI LA TABLA personas EXISTE EN NUESTRA BD
 		System.out.println("CONSULTAMOS SI LA TABLA personas EXISTE");
 		System.out.println(containsTable(con, "personas"));
 		System.out.println();
 
+		
 		// MUESTRA LA TABLA personas
 		// prepara la consulta
 		Statement stmt = con.createStatement();
-
-		// Ejecuta la consulta
-		// rs declarado arriba
+		
+		// Ejecuta la consulta (rs declarado arriba)
 		rs = stmt.executeQuery("SELECT * FROM personas");
 
 		// Muestra el ResultSet obtenido
 		showResultSet(rs);
+		
+		
+		// Cierra la conexion a la bd
+		con.close();
 
 	}
 	
@@ -61,49 +68,47 @@ public class main {
 	
 
 	// INDICA SI LA TABLA EXISTE EN NUESTRA BD
+	// Unidad 3.2 - JDBC Statements
+	// implementa una función estática para saber si
+	// el nombre de una tabla existe en una base de datos
 	public static boolean containsTable(Connection con, String tableName) throws Exception {
 		// Connection con = DriverManager.getConnection("jdbc:sqlite:sample.db");
 		// String tableName = "personas";
 		
-		if(con.isClosed() == true) {
+		if(con.isClosed() == true) { // verificacion de estar conectado a bd
 			throw new Exception("Excepcion en containsTable(): Se requiere estar conectado a la BD para llamar a esta funcion.");
 		}
 
 		boolean retorno = false;
-
 		ResultSet respuesta = con.getMetaData().getTables(null, null, tableName, null);
-
 		if (respuesta.next()) {
-			// La tabla "+ tableName +" existe en la base de datos y movemos el cursor a la
-			// primera fila del ResultSet
 			do {
 				retorno = true;
-				// System.out.println("Catalogo: " + respuesta.getString(1));
-				// System.out.println("esquema: " + respuesta.getString(2));
-				// System.out.println("tabla: " + respuesta.getString(3));
-				// System.out.println("tipo: " + respuesta.getString(4));
 			} while (respuesta.next());
-		} else {
-			// La tabla "+ tableName +" no existe en la base de datos
 		}
-
 		return retorno;
 	}
 
 
 	// MOSTRAR EL CONTENIDO DE UN ResultSet POR COLUMNAS
+	// Unidad 3.2 - JDBC Statements
+	// Implementa una función estática para mostrar el contenido de un
+	// ResultSet. Para cada fila del conjunto de resultados,
+	// imprime el contenido de cada columna
 	public static void showResultSet(ResultSet res) throws SQLException {
 		ResultSetMetaData metadata = res.getMetaData();
-		int columnas = metadata.getColumnCount();
+		int numColumnas = metadata.getColumnCount();
 
-		for (int i=1; i < columnas+1; i++) {
+		// titulos columna
+		for (int i=1; i < numColumnas+1; i++) {
 			System.out.print("  |  " + metadata.getColumnName(i) );
 		}
 		System.out.println("  |  ");
 		System.out.println();
 		
+		// campos
 		while (res.next()) {
-			for (int i=1; i < columnas+1; i++) {
+			for (int i=1; i < numColumnas+1; i++) {
 				System.out.print("  |  "+res.getString(i) ) ;
 			}
 			System.out.println("  |  ");
