72846fda4ae9372c02b18a9b7d79c130182a469e
elarreglador <elarreglador@protonmail.com>
2024-10-26 10:49:50 +0200

    Renombrado main -> JDBCHelper para copiar la lib. en otros proyectos

--- src/main/java/JDBCHelper.java
+++ src/main/java/JDBCHelper.java
@@ -6,7 +6,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class main {
+public class JDBCHelper {
 
 	public static void main(String[] args) throws Exception {
 
